1eb101ae5f8860fc60cd0d425fcf7cc2e46e4332
nikom2532 <nikom2532@hotmail.com>
2013-09-15 00:20:20 +0700

    Add lib MySQL

---
+++ Site/lib/conn.inc.php
@@ -0,0 +1,14 @@
+﻿<?
+
+	$host="localhost";
+	$username="";
+	$password="";
+	$database="";
+	$db=new DB($host,$username,$password,$database);
+          //$db->open();
+          //$db->query("");
+          //$result = $db->fetchArray();
+          //$db->freeResult();
+          //$db->close();
+          //('host','username','password','database')
+?>
