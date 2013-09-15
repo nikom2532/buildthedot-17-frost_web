b1c19b6aa8a7a527ef5e309013108f08a9b23fcf
nikom2532 <nikom2532@hotmail.com>
2013-09-15 18:11:08 +0700

    .

--- Site/lib/conn.inc.php
+++ Site/lib/conn.inc.php
@@ -1,14 +1,7 @@
-﻿<?
-
+﻿<?php
 	$host="localhost";
 	$username="";
 	$password="";
 	$database="";
 	$db=new DB($host,$username,$password,$database);
-          //$db->open();
-          //$db->query("");
-          //$result = $db->fetchArray();
-          //$db->freeResult();
-          //$db->close();
-          //('host','username','password','database')
 ?>
