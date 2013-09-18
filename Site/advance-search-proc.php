dbe1cbd8ce50234a4c22e46d95375de1da66c9bf
buildthedot1 <dev1@buildthedot.com>
2013-09-18 16:20:07 +0700

    noi

--- Site/advance-search-proc.php
+++ Site/advance-search-proc.php
@@ -29,7 +29,7 @@ else if (!empty($categoryID)){ //if keyword not set but category set then goes h
 }
 /*---------Paging------------*/
 $page=1;//Default page
-$limit=2;//Records per page
+$limit=10;//Records per page
 $start=0;//starts displaying records from 0
 if(isset($_GET['page']) && $_GET['page']!=''){
 $page=$_GET['page'];
