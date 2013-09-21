f3eb3e82288158df96dcca35cb361ecdcab879ba
nikom2532 <nikom2532@hotmail.com>
2013-09-22 00:00:18 +0700

    Reformat

--- Site/main-knowledge.php
+++ Site/main-knowledge.php
@@ -59,9 +59,6 @@ include ($rootpath."include/top-menu.php");
 									";
 								// }
 							}//end query2
-							// else{
-								// echo "a";
-							// }
 							if($temp_glvl__==3){	//find Group Level 2 ID
 								$PERMISSION_glvl2_ID = $temp_id__;
 							}
@@ -109,77 +106,11 @@ include ($rootpath."include/top-menu.php");
 					$c_UPDATE_DATE = array();
 					$c_DESCRIPTION = array();
 				 $SQLcontent="
-					SELECT * 
-					FROM  `PDF`
-					INNER JOIN `PDF_CATEGORY`  
-					WHERE `PDF_CATEGORY`.`GROUP_LEVEL_NAME` = '{$_GET["glvl"]}'
-					AND `PDF_CATEGORY`.`GROUP_LEVEL_ID` = '{$_GET["id"]}'
-					AND `PDF_CATEGORY`.`PDF_ID` = `PDF`.`ID`
-				;";
-				$resultcontent = @mysql_query($SQLcontent);
-				while ($rscontent = @mysql_fetch_array($resultcontent)) {
-					$c_ID[] = $rscontent["ID"];
-					$c_NAME[] = $rscontent["NAME"];
-					$c_UPDATE_DATE[] = $rscontent["UPDATE_DATE"];
-					$c_DESCRIPTION[] = $rscontent["DESCRIPTION"];
-				}
-				
-				//#################################
-				//####### Query 2nd List ##########
-				//#################################
-				$temp_glvl_content = $_GET["glvl"];
-				$temp_id_content = $_GET["id"];
-				/*
-				//############## Find Parents ################
-				
-				while ($temp_glvl_content > 2) {
-					$SQLcontent="
 						SELECT * 
 						FROM  `PDF`
 						INNER JOIN `PDF_CATEGORY`  
-						WHERE `PDF_CATEGORY`.`GROUP_LEVEL_NAME` = '".($temp_glvl_content-1)."'
-						AND `PDF_CATEGORY`.`GROUP_LEVEL_ID` = 
-						(
-							SELECT `GROUP_LV".($temp_glvl_content-1)."`.`ID`
-							FROM  `GROUP_LV{$temp_glvl_content}` 
-							INNER JOIN  `GROUP_LV".($temp_glvl_content-1)."` 
-							WHERE  `GROUP_LV".($temp_glvl_content)."`.`GROUP_LV".($temp_glvl_content-1)."_ID` =  `GROUP_LV".($temp_glvl_content-1)."`.`ID` 
-							AND  `GROUP_LV".($temp_glvl_content)."`.`ID` =  '{$temp_id_content}'
-						)
-						AND `PDF_CATEGORY`.`PDF_ID` = `PDF`.`ID`
-					;";	
-					$resultcontent = @mysql_query($SQLcontent);
-					while ($rscontent = @mysql_fetch_array($resultcontent)) {
-						$c_NAME[] = $rscontent["NAME"];
-						$c_UPDATE_DATE[] = $rscontent["UPDATE_DATE"];
-						$c_DESCRIPTION[] = $rscontent["DESCRIPTION"];
-					}
-					$SQLcontent2="
-						SELECT * 
-						FROM  `GROUP_LV{$temp_glvl_content}`
-						WHERE `ID` = '{$temp_id_content}'
-					";
-					$resultcontent2 = @mysql_query($SQLcontent2);
-					while ($rscontent2 = @mysql_fetch_array($resultcontent2)) {
-						$temp_id_content = $rscontent2["GROUP_LV".($temp_glvl_content-1)."_ID"];
-					}
-					$temp_glvl_content--;
-				}//end while
-				*/
-				
-				//############## Find Children ####################
-				while ($temp_glvl_content < 6) {
-					$SQLcontent="
-						SELECT * 
-						FROM  `PDF`
-						INNER JOIN `PDF_CATEGORY`  
-						WHERE `PDF_CATEGORY`.`GROUP_LEVEL_NAME` = '".($temp_glvl_content+1)."'
-						AND `PDF_CATEGORY`.`GROUP_LEVEL_ID` IN 
-						(
-							SELECT `GROUP_LV".($temp_glvl_content+1)."`.`ID`
-							FROM  `GROUP_LV".($temp_glvl_content+1)."` 
-							WHERE  `GROUP_LV{$temp_glvl_content}_ID` = '{$temp_id_content}'
-						)
+						WHERE `PDF_CATEGORY`.`GROUP_LEVEL_NAME` = '{$_GET["glvl"]}'
+						AND `PDF_CATEGORY`.`GROUP_LEVEL_ID` = '{$_GET["id"]}'
 						AND `PDF_CATEGORY`.`PDF_ID` = `PDF`.`ID`
 					;";
 					$resultcontent = @mysql_query($SQLcontent);
@@ -189,149 +120,214 @@ include ($rootpath."include/top-menu.php");
 						$c_UPDATE_DATE[] = $rscontent["UPDATE_DATE"];
 						$c_DESCRIPTION[] = $rscontent["DESCRIPTION"];
 					}
-					$SQLcontent2="
-						SELECT * 
-						FROM  `GROUP_LV{$temp_glvl_content}`
-						WHERE `ID` = '{$temp_id_content}'
-					";
-					$resultcontent2 = @mysql_query($SQLcontent2);
-					while ($rscontent2 = @mysql_fetch_array($resultcontent2)) {
-						$temp_id_content = $rscontent2["GROUP_LV".($temp_glvl_content-1)."_ID"];
+				
+					//#################################
+					//####### Query 2nd List ##########
+					//#################################
+					$temp_glvl_content = $_GET["glvl"];
+					$temp_id_content = $_GET["id"];
+					/*
+					//############## Find Parents ################
+				
+					while ($temp_glvl_content > 2) {
+						$SQLcontent="
+							SELECT * 
+							FROM  `PDF`
+							INNER JOIN `PDF_CATEGORY`  
+							WHERE `PDF_CATEGORY`.`GROUP_LEVEL_NAME` = '".($temp_glvl_content-1)."'
+							AND `PDF_CATEGORY`.`GROUP_LEVEL_ID` = 
+							(
+								SELECT `GROUP_LV".($temp_glvl_content-1)."`.`ID`
+								FROM  `GROUP_LV{$temp_glvl_content}` 
+								INNER JOIN  `GROUP_LV".($temp_glvl_content-1)."` 
+								WHERE  `GROUP_LV".($temp_glvl_content)."`.`GROUP_LV".($temp_glvl_content-1)."_ID` =  `GROUP_LV".($temp_glvl_content-1)."`.`ID` 
+								AND  `GROUP_LV".($temp_glvl_content)."`.`ID` =  '{$temp_id_content}'
+							)
+							AND `PDF_CATEGORY`.`PDF_ID` = `PDF`.`ID`
+						;";	
+						$resultcontent = @mysql_query($SQLcontent);
+						while ($rscontent = @mysql_fetch_array($resultcontent)) {
+							$c_NAME[] = $rscontent["NAME"];
+							$c_UPDATE_DATE[] = $rscontent["UPDATE_DATE"];
+							$c_DESCRIPTION[] = $rscontent["DESCRIPTION"];
+						}
+						$SQLcontent2="
+							SELECT * 
+							FROM  `GROUP_LV{$temp_glvl_content}`
+							WHERE `ID` = '{$temp_id_content}'
+						";
+						$resultcontent2 = @mysql_query($SQLcontent2);
+						while ($rscontent2 = @mysql_fetch_array($resultcontent2)) {
+							$temp_id_content = $rscontent2["GROUP_LV".($temp_glvl_content-1)."_ID"];
+						}
+						$temp_glvl_content--;
+					}//end while
+					*/
+				
+					//############## Find Children ####################
+					while ($temp_glvl_content < 6) {
+						$SQLcontent="
+							SELECT * 
+							FROM  `PDF`
+							INNER JOIN `PDF_CATEGORY`  
+							WHERE `PDF_CATEGORY`.`GROUP_LEVEL_NAME` = '".($temp_glvl_content+1)."'
+							AND `PDF_CATEGORY`.`GROUP_LEVEL_ID` IN 
+							(
+								SELECT `GROUP_LV".($temp_glvl_content+1)."`.`ID`
+								FROM  `GROUP_LV".($temp_glvl_content+1)."` 
+								WHERE  `GROUP_LV{$temp_glvl_content}_ID` = '{$temp_id_content}'
+							)
+							AND `PDF_CATEGORY`.`PDF_ID` = `PDF`.`ID`
+						;";
+						$resultcontent = @mysql_query($SQLcontent);
+						while ($rscontent = @mysql_fetch_array($resultcontent)) {
+							$c_ID[] = $rscontent["ID"];
+							$c_NAME[] = $rscontent["NAME"];
+							$c_UPDATE_DATE[] = $rscontent["UPDATE_DATE"];
+							$c_DESCRIPTION[] = $rscontent["DESCRIPTION"];
+						}
+						$SQLcontent2="
+							SELECT * 
+							FROM  `GROUP_LV{$temp_glvl_content}`
+							WHERE `ID` = '{$temp_id_content}'
+						";
+						$resultcontent2 = @mysql_query($SQLcontent2);
+						while ($rscontent2 = @mysql_fetch_array($resultcontent2)) {
+							$temp_id_content = $rscontent2["GROUP_LV".($temp_glvl_content-1)."_ID"];
+						}
+						$temp_glvl_content++;
+					}//end while
+					
+					//#####################################
+					//Display List content from Above query
+					//#####################################
+					
+					//Start Page = 1
+					if(!isset($_GET["page"])){
+						$_GET["page"]=1;
 					}
-					$temp_glvl_content++;
-				}//end while
+					$page = $_GET["page"];
+					$page_limit = 10;
+					$number_of_items = count($c_NAME);
+					$number_of_pages = ((int)($number_of_items/$page_limit)) + 1; 
+					
+					if($number_of_pages==$page){	//means the last page
+						$page_runing = $number_of_items;
+					}
+					else{	//not the last page
+						$page_runing = $page_limit*$page;
+					}
 				
-				//#####################################
-				//Display List content from Above query
-				//#####################################
-				
-				//Start Page = 1
-				if(!isset($_GET["page"])){
-					$_GET["page"]=1;
-				}
-				$page = $_GET["page"];
-				$page_limit = 10;
-				$number_of_items = count($c_NAME);
-				$number_of_pages = ((int)($number_of_items/$page_limit)) + 1; 
-				
-				if($number_of_pages==$page){	//means the last page
-					$page_runing = $number_of_items;
-				}
-				else{	//not the last page
-					$page_runing = $page_limit*$page;
-				}
-				
-				//for ($i=0; $i < count($c_NAME); $i++) {	//for all Pages
-				for ($i = ($page_limit*($page-1)); $i < $page_runing; $i++) { //for each Page
+					//for ($i=0; $i < count($c_NAME); $i++) {	//for all Pages
+					for ($i = ($page_limit*($page-1)); $i < $page_runing; $i++) { //for each Page
 ?>
-					<section>
-						<p class="bold text-title-report">
-							<span class="text-lightgreen head-desc">Title: </span>
-							<a href="<?php echo $rootpath; ?>report-detail.php?id=<?php echo $c_ID["$i"]; ?>&glvl=<?php echo $_GET["glvl"]; ?>">
-								<?php echo $c_NAME["$i"]; ?>
-								<span id="ic-lock">
+						<section>
+							<p class="bold text-title-report">
+								<span class="text-lightgreen head-desc">Title: </span>
+								<a href="<?php echo $rootpath; ?>report-detail.php?id=<?php echo $c_ID["$i"]; ?>&glvl=<?php echo $_GET["glvl"]; ?>">
+									<?php echo $c_NAME["$i"]; ?>
+									<span id="ic-lock">
 <?php
-									if($PERMISSION_Is_Lockkey=="Y"){
+										if($PERMISSION_Is_Lockkey=="Y"){
 ?>
-										<img src="images/icons/ic_lock.png" width="16" height="16">
+											<img src="images/icons/ic_lock.png" width="16" height="16">
 <?php
-									}
+										}
 ?>
-								</span>
-							</a>
-						</p>
-						<p>
-							<span class="text-lightgreen bold head-desc">Update: </span><span class="date"><?php echo convertDate2String($c_UPDATE_DATE["$i"]); ?></span>
-						</p>
-						<p class="text-desc">
-							<span class="text-lightgreen bold head-desc">Description: </span><?php echo $c_DESCRIPTION["$i"]; ?>
-						</p>
-					</section>
+									</span>
+								</a>
+							</p>
+							<p>
+								<span class="text-lightgreen bold head-desc">Update: </span><span class="date"><?php echo convertDate2String($c_UPDATE_DATE["$i"]); ?></span>
+							</p>
+							<p class="text-desc">
+								<span class="text-lightgreen bold head-desc">Description: </span><?php echo $c_DESCRIPTION["$i"]; ?>
+							</p>
+						</section>
 <?php
 				}
-				
-				//#############################
-				//####### test ##########
-				//echo $_SESSION["userid"];
-				// print_r($c_ID);
-				// print_r($c_NAME);
-				//#############################
-				
-				
-				//############ Paging ############
-				if($number_of_pages>1){
+					
+					//#############################
+					//####### test ##########
+					//echo $_SESSION["userid"];
+					// print_r($c_ID);
+					// print_r($c_NAME);
+					//#############################
+					
+					
+					//############ Paging ############
+					if($number_of_pages>1){
 ?>
-					<ul class="pagination">
-						<li class="details">Page <?php echo $_GET["page"]; ?> of <?php echo $number_of_pages; ?></li>
+						<ul class="pagination">
+							<li class="details">Page <?php echo $_GET["page"]; ?> of <?php echo $number_of_pages; ?></li>
 <?php
-						for($i=1;$i<=$number_of_pages;$i++){	//Page Button
+							for($i=1;$i<=$number_of_pages;$i++){	//Page Button
 ?>
-							<li><a href="main-knowledge.php?id=<?php echo $_GET["id"]; ?>&glvl=<?php echo $_GET["glvl"]; ?>&page=<?php echo $i; ?>" <?php if($page==$i){ ?>class="current" <?php } ?>><?php echo $i; ?></a></li>
+								<li><a href="main-knowledge.php?id=<?php echo $_GET["id"]; ?>&glvl=<?php echo $_GET["glvl"]; ?>&page=<?php echo $i; ?>" <?php if($page==$i){ ?>class="current" <?php } ?>><?php echo $i; ?></a></li>
 <?php
-						}
-						if($_GET["page"]<$number_of_pages){	//Next Button
+							}
+							if($_GET["page"]<$number_of_pages){	//Next Button
 ?>
-							<li><a href="main-knowledge.php?id=<?php echo $_GET["id"]; ?>&glvl=<?php echo $_GET["glvl"]; ?>&page=<?php echo ($_GET["page"]+1); ?>">Next</a></li>
+								<li><a href="main-knowledge.php?id=<?php echo $_GET["id"]; ?>&glvl=<?php echo $_GET["glvl"]; ?>&page=<?php echo ($_GET["page"]+1); ?>">Next</a></li>
 <?php
-						}
+							}
 ?>
-					</ul>
+						</ul>
 <?php
-				}
+					}
 ?>
-				<!-- <section>
-					<p class="bold">
-						<span class="text-lightgreen head-desc">Title: </span><a href="#">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<span id="ic-lock"><img src="images/icons/ic_lock.png" width="16" height="16"></span></a>
-					</p>
-					<p>
-						<span class="text-lightgreen bold head-desc">Update: </span><span class="date">Sep, 16 2013</span>
-					</p>
-					<p>
-						<span class="text-lightgreen bold head-desc">Description: </span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-					</p>
-				</section> -->
-
-				<!-- <div class="grid_12" id="page-num">
-					<ul class="left">
-						<li class="active-page">
-							<a href="#">1</a>
-						</li>
-						<li>
-							<a href="#">2</a>
-						</li>
-						<li>
-							<a href="#">3</a>
-						</li>
-						<li>
-							<a href="#">4</a>
-						</li>
-						<li>
-							<a href="#">5</a>
-						</li>
-						<li>
-							<a href="#">6</a>
-						</li>
-						<li>
-							<a href="#">7</a>
-						</li>
-						<li>
-							<a href="#">8</a>
-						</li>
-						<li>
-							<a href="#">></a>
-						</li>
-						<li>
-							<a href="#">>></a>
-						</li>
-					</ul>
-				</div> -->
-				<!--end page num -->
-
-			</div><!--end content-middle -->
-		</div>
-	</div><!--end container_12 -->
-</div><!--end content -->
+						<!-- <section>
+							<p class="bold">
+								<span class="text-lightgreen head-desc">Title: </span><a href="#">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<span id="ic-lock"><img src="images/icons/ic_lock.png" width="16" height="16"></span></a>
+							</p>
+							<p>
+								<span class="text-lightgreen bold head-desc">Update: </span><span class="date">Sep, 16 2013</span>
+							</p>
+							<p>
+								<span class="text-lightgreen bold head-desc">Description: </span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+							</p>
+						</section> -->
+						<div class="grid_12" id="page-num">
+							<ul class="left">
+								<li class="active-page">
+									<a href="#">1</a>
+								</li>
+								<li>
+									<a href="#">2</a>
+								</li>
+								<li>
+									<a href="#">3</a>
+								</li>
+								<li>
+									<a href="#">4</a>
+								</li>
+								<li>
+									<a href="#">5</a>
+								</li>
+								<li>
+									<a href="#">6</a>
+								</li>
+								<li>
+									<a href="#">7</a>
+								</li>
+								<li>
+									<a href="#">8</a>
+								</li>
+								<li>
+									<a href="#">></a>
+								</li>
+								<li>
+									<a href="#">>></a>
+								</li>
+							</ul>
+						</div>
+						<!--end page num -->
+						
+					</div><!--end content-middle -->
+				</div>
+			</div><!--end container_12 -->
+		</div><!--end content -->
 
 <?php
-include ("include/footer.php");
+		include ("include/footer.php");
 ?>
