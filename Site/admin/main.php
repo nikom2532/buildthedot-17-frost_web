f310b0b2eef4aaea34cb0e3c13d974800112eeea
nikom2532 <nikom2532@hotmail.com>
2013-10-03 02:36:37 +0700

    Reformat: admin/main.php

--- Site/admin/main.php
+++ Site/admin/main.php
@@ -1,63 +1,79 @@
-<?php include("include/header.php");?>
-<?php include("include/top-bar.php");?>
-<?php include("include/checksession.php");?>
-	<!-- HEADER -->
-	<div id="header-with-tabs">
-		
-		<div class="page-full-width cf">
-	
-			<ul id="tabs" class="left">
-				<li><a href="main.php" class="active-tab dashboard-tab">Dashboard</a></li>
-				<li><a href="customer.php">Customer Management</a></li>
-				<li><a href="pdf.php">PDF Management</a></li>
-                <li><a href="tag.php">Tag Management</a></li>
-			</ul> <!-- end tabs -->
-			
-			<!-- company logo -->
-			<a href="#" id="company-branding-small" class="right"><img src="images/mckansys_logo.png" width="200" height="27" alt="logo"></a>
-			
-		</div> <!-- end full-width -->	
+<?php
+include ("include/header.php");
+?>
+<?php
+	include ("include/top-bar.php");
+?>
+<?php
+	include ("include/checksession.php");
+?>
+<!-- HEADER -->
+<div id="header-with-tabs">
 
-	</div> <!-- end header -->
-		
-	<!-- MAIN CONTENT -->
-	<div id="content">
-		
-		<div class="page-full-width cf">
+	<div class="page-full-width cf">
 
-			<div class="content-module">
-			
-				<div class="content-module-heading cf">
-				
-					<h3 class="fl">Top five downloads </h3>
-					<span class="fr expand-collapse-text">Click to collapse</span>
-					<span class="fr expand-collapse-text initial-expand">Click to expand</span>
-				
-				</div> <!-- end content-module-heading -->
-				
-				
-				<div class="content-module-main">
-				
-					<h2>Group by user</h2>
-					
-					<table class="fixed">
-						<col width="2em" />
-	    				 <col width="20em" />
-	    				 <col width="5em" />
-					
-						<thead>
-					
-							<tr>
-								<th>No.</th>
-								<th>Name</th>
-								<th>Total Downloaded</th>
-							</tr>
-						
-						</thead>
+		<ul id="tabs" class="left">
+			<li>
+				<a href="main.php" class="active-tab dashboard-tab">Dashboard</a>
+			</li>
+			<li>
+				<a href="customer.php">Customer Management</a>
+			</li>
+			<li>
+				<a href="pdf.php">PDF Management</a>
+			</li>
+			<li>
+				<a href="tag.php">Tag Management</a>
+			</li>
+		</ul>
+		<!-- end tabs -->
 
-						<tfoot>
-						
-						
+		<!-- company logo -->
+		<a href="#" id="company-branding-small" class="right"><img src="images/mckansys_logo.png" width="200" height="27" alt="logo"></a>
+
+	</div>
+	<!-- end full-width -->
+
+</div>
+<!-- end header -->
+
+<!-- MAIN CONTENT -->
+<div id="content">
+
+	<div class="page-full-width cf">
+
+		<div class="content-module">
+
+			<div class="content-module-heading cf">
+
+				<h3 class="fl">Top five downloads </h3>
+				<span class="fr expand-collapse-text">Click to collapse</span>
+				<span class="fr expand-collapse-text initial-expand">Click to expand</span>
+
+			</div>
+			<!-- end content-module-heading -->
+
+			<div class="content-module-main">
+
+				<h2>Group by user</h2>
+
+				<table class="fixed">
+					<col width="2em" />
+					<col width="20em" />
+					<col width="5em" />
+
+					<thead>
+
+						<tr>
+							<th>No.</th>
+							<th>Name</th>
+							<th>Total Downloaded</th>
+						</tr>
+
+					</thead>
+
+					<tfoot>
+
 						<tbody>
 
 							<tr>
@@ -90,32 +106,33 @@
 								<td>10</td>
 							</tr>
 						</tbody>
-						
-					</table>
-                   	<a href="#" class="round button orange ic-download image-left">Download Report</a>
-					<div class="stripe-separator"><!--  --></div>
-					<!--end group by user -->
-					
-                    <h2>Group by title</h2>
-					
-					<table class="fixed">
-						<col width="2em" />
-	    				<col width="20em" />
-	    				<col width="5em" />
-					
-						<thead>
-					
-							<tr>
-								<th>No.</th>
-								<th>Title</th>
-								<th>Total Downloaded</th>
-							</tr>
-						
-						</thead>
 
-						<tfoot>
-						
-						
+				</table>
+				<a href="#" class="round button orange ic-download image-left">Download Report</a>
+				<div class="stripe-separator">
+					<!--  -->
+				</div>
+				<!--end group by user -->
+
+				<h2>Group by title</h2>
+
+				<table class="fixed">
+					<col width="2em" />
+					<col width="20em" />
+					<col width="5em" />
+
+					<thead>
+
+						<tr>
+							<th>No.</th>
+							<th>Title</th>
+							<th>Total Downloaded</th>
+						</tr>
+
+					</thead>
+
+					<tfoot>
+
 						<tbody>
 
 							<tr>
@@ -148,17 +165,23 @@
 								<td>10</td>
 							</tr>
 						</tbody>
-						
-					</table>
-                   	<a href="#" class="round button orange ic-download image-left">Download Report</a>
-				
-					<!--end group by user -->
-				</div> <!-- end content-module-main -->
-			
-			</div> <!-- end content-module -->
-		
-		</div> <!-- end full-width -->
-			
-	</div> <!-- end content -->
-	
-<?php include("include/footer.php");?>
+
+				</table>
+				<a href="#" class="round button orange ic-download image-left">Download Report</a>
+
+				<!--end group by user -->
+			</div>
+			<!-- end content-module-main -->
+
+		</div>
+		<!-- end content-module -->
+
+	</div>
+	<!-- end full-width -->
+
+</div>
+<!-- end content -->
+
+<?php
+	include ("include/footer.php");
+?>
