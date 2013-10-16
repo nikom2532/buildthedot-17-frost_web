9e86f324f49fffd2e638de21e921b60a59385136
nikom2532 <nikom2532@hotmail.com>
2013-10-16 20:46:35 +0700

    12. เพิม function - 1 user สามารถดาวโหลดได ้ไม่เกิน 5 report ต่อวัน

--- Site/report-detail.php
+++ Site/report-detail.php
@@ -1,7 +1,6 @@
 <?php
 $rootpath = "./";
 include ("include/header.php");
-
 include ("include/top-menu.php");
 ?>
 <div id="content">
@@ -150,80 +149,95 @@ include ("include/top-menu.php");
 <?php
 					
 ?>
-						<!-- Download --><?php 
-							//####### Find Lock Download Key ##########
-							$temp_id__ = $_GET["id"];
-							$temp_glvl__ = $_GET["glvl"];
-							$str = array();
-							while ($temp_glvl__ >= 2) {
-								$SQLnav = "
-									SELECT *
-									FROM `GROUP_LV" . $temp_glvl__ . "`
-									WHERE `ID` = '" . $temp_id__ . "'
-								;";
-								$resultnav = @mysql_query($SQLnav);
-								if($rsnav = @mysql_fetch_array($resultnav)) {
-									$temp_id__ = $rsnav["GROUP_LV" . ($temp_glvl__ - 1) . "_ID"];
-									if($temp_glvl__==3){	//find Group Level 2 ID
-										$PERMISSION_glvl2_ID = $temp_id__;
-									}
-									$temp_glvl__--;
-								}//end query
-							}//end while
-							$PERMISSION_Is_Lockkey="";
-							if(!$PERMISSION_glvl2_ID){
-								if($_GET["glvl"]==2){
-									$PERMISSION_glvl2_ID = $_GET["id"];
-								}
+					<?php //### Download ####
+					//####### Find Lock Download Key ##########
+					$temp_id__ = $_GET["id"];
+					$temp_glvl__ = $_GET["glvl"];
+					$str = array();
+					while ($temp_glvl__ >= 2) {
+						$SQLnav = "
+							SELECT *
+							FROM `GROUP_LV" . $temp_glvl__ . "`
+							WHERE `ID` = '" . $temp_id__ . "'
+						;";
+						$resultnav = @mysql_query($SQLnav);
+						if($rsnav = @mysql_fetch_array($resultnav)) {
+							$temp_id__ = $rsnav["GROUP_LV" . ($temp_glvl__ - 1) . "_ID"];
+							if($temp_glvl__==3){	//find Group Level 2 ID
+								$PERMISSION_glvl2_ID = $temp_id__;
 							}
-							$SQLlockKey="
-								SELECT * 
-								FROM  `PERMISSION`
-								WHERE `USER_PROFILE_ID` = '{$_SESSION["userid"]}'
-								AND `GROUP_LV2_ID` = '{$PERMISSION_glvl2_ID}'
-								AND `IS_ACTIVE` = 'Y'
-								AND `END_DATE` > NOW()
-							;";
-							$resultLockKey = @mysql_query($SQLlockKey);
-							if($rsLockKey = @mysql_fetch_array($resultLockKey)) {
-								$PERMISSION_Is_Lockkey = "N";
-							}
-							else{
-								$PERMISSION_Is_Lockkey = "Y";
-							}
-							if($PERMISSION_Is_Lockkey=="Y"){
-								//echo "<a href='#'>";
+							$temp_glvl__--;
+						}//end query
+					}//end while
+					$PERMISSION_Is_Lockkey="";
+					if(!$PERMISSION_glvl2_ID){
+						if($_GET["glvl"]==2){
+							$PERMISSION_glvl2_ID = $_GET["id"];
+						}
+					}
+					$SQLlockKey="
+						SELECT * 
+						FROM  `PERMISSION`
+						WHERE `USER_PROFILE_ID` = '{$_SESSION["userid"]}'
+						AND `GROUP_LV2_ID` = '{$PERMISSION_glvl2_ID}'
+						AND `IS_ACTIVE` = 'Y'
+						AND `END_DATE` > NOW()
+					;";
+					$resultLockKey = @mysql_query($SQLlockKey);
+					if($rsLockKey = @mysql_fetch_array($resultLockKey)) {
+						$PERMISSION_Is_Lockkey = "N";
+					}
+					else{
+						$PERMISSION_Is_Lockkey = "Y";
+					}
+					if($PERMISSION_Is_Lockkey=="Y"){ //#### if you cannot download ####
 ?>
-								<b class="button grey" id="disable-button">
+						<b class="button grey" id="disable-button">
 <?php
-								echo "Download";// paid
+						echo "Download";// paid
 ?>
-								</b>
+						</b>
+<?php
+					}
+					elseif($PERMISSION_Is_Lockkey=="N"){ //#### if you can download ####
+												
+						$SQL_Is_download_5_pdfs_a_day="
+							SELECT COUNT(`PDF_ID`) AS Download_Count
+							FROM `DOWNLOAD_STATISTICS`
+							WHERE `USER_ID` = '".$_SESSION["userid"]."'
+							AND DATE(`DOWNLOAD_DATETIME`) = DATE(NOW())
+						;";
+						$result_Is_download_5_pdfs_a_day = @mysql_query($SQL_Is_download_5_pdfs_a_day);
+						while($rs_Is_download_5_pdfs_a_day = @mysql_fetch_array($result_Is_download_5_pdfs_a_day)) {
+							if($rs_Is_download_5_pdfs_a_day["Download_Count"]>5) { //if user download more than 5 Downloads on 1 day.
+								 
+?>
+								<a href="#" onclick="window.alert(\"You download more than 5 times a day\");">
+									<b class="button darkgreen" id="download-button">Download</b>
+								</a>
 <?php
 							}
-							elseif($PERMISSION_Is_Lockkey=="N"){
-								echo "<a href='download-pdf.php?pdfId=". $id ."'>";
+							else{ //if user download less than 5 Downloads on 1 day.
 ?>
-								<b class="button darkgreen" id="download-button">
-<?php
-								echo "Download";
-?>
-								</b>
+								<a href="download-pdf.php?pdfId=<?php echo $id; ?>">
+									<b class="button darkgreen" id="download-button">
+										Download
+									</b>
+								</a>
 <?php
 							}
-						?>
-					</a>
+						}
+					}
+?>
 				</div>
-			
-				<?php } ?>
-				
-				
+<?php
+				}
+?>
 			</div><!--end content-middle -->
 		<!-- </div> -->
 	</div><!--end container_12 -->
 </div><!--end content -->
 
 <?php
-include ("include/footer.php");
-
+include("include/footer.php");
 ?>
