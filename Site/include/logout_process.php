744d601dc2fb6f02258f2a200ee03a6a30afb58b
nikom2532 <nikom2532@hotmail.com>
2013-10-24 17:07:49 +0700

    Front-End: Fix Bug Login

--- Site/include/logout_process.php
+++ Site/include/logout_process.php
@@ -1,6 +1,12 @@
 ﻿<?php
 include($rootpath."include/header.php");
 unset($_SESSION["userid"]);
-include($rootpath."include/footer.php");;
-header("Location: ".$rootpath.".".urldecode($_GET["pa"]));
+include($rootpath."include/footer.php");
+
+if(strstr(urldecode($_GET["pa"]), '?', true) == "/myprofile.php"){
+	header("Location: ".$rootpath."./index.php");
+}
+else{
+	header("Location: ".$rootpath.".".urldecode($_GET["pa"]));
+}
 ?>
