dd9b2a624dbf56625c7e73c9621cf1a5a91619cf
nikom2532 <nikom2532@hotmail.com>
2013-10-24 18:17:40 +0700

    .

--- Site/include/logout_process.php
+++ Site/include/logout_process.php
@@ -3,7 +3,8 @@ include($rootpath."include/header.php");
 unset($_SESSION["userid"]);
 include($rootpath."include/footer.php");
 
-if(strstr(urldecode($_GET["pa"]), '?', true) == "/myprofile.php"){
+//If you are on Profile Page
+if(strstr(urldecode($_GET["pa"]), '?', true) == "myprofile.php"){
 	header("Location: ".$rootpath."./index.php");
 }
 else{
