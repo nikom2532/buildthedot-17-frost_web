b1b512b59445bf32542ad0aabed09c556c53fe32
buildthedot1 <dev1@buildthedot.com>
2013-10-25 18:20:05 +0700

    fix strategy link

--- Site/index.php
+++ Site/index.php
@@ -48,7 +48,7 @@ include ("include/top-menu.php");
 				</p> </a>
 			</div>
 			<div class="grid_3 center">
-				<a href="<?php echo $rootpath; ?>main-knowledge.php?id=3&glvl=2&gp=<?php 
+				<a href="<?php echo $rootpath; ?>main-knowledge.php?id=2&glvl=2&gp=<?php 
 					$SQL2="
 						SELECT * 
 						FROM  `GROUP_LV3`
