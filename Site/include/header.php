d5105ca3a7c7b53ddc732c3903fb6b864da8626f
nikom2532 <nikom2532@hotmail.com>
2013-10-29 14:12:03 +0700

    move the position of init DB.

--- Site/include/header.php
+++ Site/include/header.php
@@ -27,4 +27,14 @@
 
 		</script>
 	</head>
+		<?php
+		session_start();
+		include ($rootpath . "lib/db.php");
+		//include ($rootpath."lib/connect-db-fon.php");
+		include ($rootpath . "lib/conn.inc.php");
+		include ($rootpath . "lib/func_date.php");
+		if (!$db -> open()) {
+			die($db -> error());
+		}
+		?>
 	<body onload="noBack();" onpageshow="if (event.persisted) noBack();" onunload="">
