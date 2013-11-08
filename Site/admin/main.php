f744abef5a980d41dd7a2d9e5c0d1698dd785d1b
nikom2532 <nikom2532@hotmail.com>
2013-11-08 17:53:04 +0700

    Reformat code

--- Site/admin/main.php
+++ Site/admin/main.php
@@ -54,70 +54,71 @@ include ("include/header.php");
 			</div>
 			<!-- end content-module-heading -->
 
+			
 			<div class="content-module-main">
 				<h2>Group by user</h2>
-					<table class="fixed">
-						<col width="2em" />
-						<col width="20em" />
-						<col width="5em" />
-	
-						<thead>
-	
-							<tr>
-								<th>No.</th>
-								<th>Name</th>
-								<th>Total Downloaded</th>
-							</tr>
-	
-						</thead>
-	
-						<tfoot>
-							
-							<tbody>
+				<table class="fixed">
+					<col width="2em" />
+					<col width="20em" />
+					<col width="5em" />
+
+					<thead>
+
+						<tr>
+							<th>No.</th>
+							<th>Name</th>
+							<th>Total Downloaded</th>
+						</tr>
+
+					</thead>
+
+					<tfoot>
+						
+						<tbody>
 <?php
-								$i=1;
-								$sql_statistic_by_user="
-									SELECT *
-									FROM  `DOWNLOAD_STATISTICS`
-									GROUP BY `USER_ID`
-									ORDER BY `USER_ID`
-									LIMIT 0,5;
-								";
-								$Result_statistic_by_user=@mysql_query($sql_statistic_by_user);
-								while($rs_statistic_by_user=@mysql_fetch_array($Result_statistic_by_user)){
+							$i=1;
+							$sql_statistic_by_user="
+								SELECT *
+								FROM  `DOWNLOAD_STATISTICS`
+								GROUP BY `USER_ID`
+								ORDER BY `USER_ID`
+								LIMIT 0,5;
+							";
+							$Result_statistic_by_user=@mysql_query($sql_statistic_by_user);
+							while($rs_statistic_by_user=@mysql_fetch_array($Result_statistic_by_user)){
 ?>
-									<tr>
-										<td><?php echo $i; ?></td>
-										<td><?php 
-											$sql_user="
-												SELECT * 
-												FROM  `USER_PROFILE`
-												WHERE `ID` = '".$rs_statistic_by_user["USER_ID"]."';
-											";
-											$result_user=@mysql_query($sql_user);
-											if($rs_user=@mysql_fetch_array($result_user)){
-												echo $rs_user["FIRSTNAME"]." ".$rs_user["LASTNAME"];
-											}
-										?></td>
-										<td><?php
-											$sql_statistic_each_user="
-												SELECT COUNT(`USER_ID`) AS `number_user`
-												FROM  `DOWNLOAD_STATISTICS`
-												WHERE `USER_ID` = '".$rs_statistic_by_user["USER_ID"]."';
-											";
-											$Result_statistic_each_user=@mysql_query($sql_statistic_each_user);
-											if($rs_statistic_each_user=@mysql_fetch_array($Result_statistic_each_user)){
-												echo $rs_statistic_each_user["number_user"];
-											}
-										?></td>
-									</tr>
+								<tr>
+									<td><?php echo $i; ?></td>
+									<td><?php 
+										$sql_user="
+											SELECT * 
+											FROM  `USER_PROFILE`
+											WHERE `ID` = '".$rs_statistic_by_user["USER_ID"]."';
+										";
+										$result_user=@mysql_query($sql_user);
+										if($rs_user=@mysql_fetch_array($result_user)){
+											echo $rs_user["FIRSTNAME"]." ".$rs_user["LASTNAME"];
+										}
+									?></td>
+									<td><?php
+										$sql_statistic_each_user="
+											SELECT COUNT(`USER_ID`) AS `number_user`
+											FROM  `DOWNLOAD_STATISTICS`
+											WHERE `USER_ID` = '".$rs_statistic_by_user["USER_ID"]."';
+										";
+										$Result_statistic_each_user=@mysql_query($sql_statistic_each_user);
+										if($rs_statistic_each_user=@mysql_fetch_array($Result_statistic_each_user)){
+											echo $rs_statistic_each_user["number_user"];
+										}
+									?></td>
+								</tr>
 <?php
-									$i++;
-								}
+								$i++;
+							}
 ?>
-							</tbody>
-						</tfoot>
-					</table>
+						</tbody>
+					</tfoot>
+				</table>
 <?php
 				//Create the CSV Report
 ?>
