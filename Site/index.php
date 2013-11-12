cf86724e18e31ca5b290b371cb0667194b5b925d
buildthedot1 <dev1@buildthedot.com>
2013-11-12 17:37:38 +0700

    update index

--- Site/index.php
+++ Site/index.php
@@ -7,6 +7,24 @@ include ("include/top-menu.php");
 	<div class="container_12" id="container">
 		<h1 id="head-title" class="text-orange uppercase grid_12">Welcome to Mckansys</h1>
 		<br class="clear"/>
+		<?php 
+		$SQLINFO="
+					SELECT * 
+					FROM  `INFO`
+					WHERE `ID` = '1'
+					";
+					$resultInfo =@mysql_query($SQLINFO);
+					while($rs1 =@mysql_fetch_array($resultInfo)){		
+					
+		?>
+		<div class="indent">
+			
+			<?php echo  $rs1['DESCRIPTION']?>
+			
+		</div>
+		<?php 
+					}
+		?>
 		<div id="icon">
 			<div class="grid_3 center">
 				<!-- <?php echo $rootpath; ?>main-knowledge.php?id=2 -->
@@ -102,7 +120,8 @@ include ("include/top-menu.php");
 				a.NAME as name,
 				a.DESCRIPTION as description,
 				b.GROUP_LEVEL_NAME as glvl,
-				b.GROUP_LEVEL_ID as glvId
+				b.GROUP_LEVEL_ID as glvId,
+				a.UPDATE_DATE as date
 				FROM PDF AS a
 				INNER JOIN PDF_CATEGORY AS b
 				ON a.ID = b.PDF_ID
@@ -113,15 +132,237 @@ include ("include/top-menu.php");
 			$result = @mysql_query($sql_new_release) or die(mysql_error());
 			?> 
 			
-			<h2 class="text-lightgreen2 uppercase">New release <?php echo $current_month_all;?>, <?php echo $current_year_all;?></h2>
+			<h2 class="text-lightgreen2 uppercase">LATEST UPDATES</h2>
 			<?php
 			//-- Add by Fon
+
+						
 			while ($row = @mysql_fetch_array($result)) {
+				// query group level 2
+				$pdfId = $row['id'];
+				$resultPdf = mysql_query("
+				SELECT *
+				FROM PDF_CATEGORY
+				WHERE PDF_ID = '$pdfId'				
+			   ");
+						   
+				while ($rowPdf = mysql_fetch_array($resultPdf)) {
+					$lvName = $rowPdf['GROUP_LEVEL_NAME'];
+					$lvId = $rowPdf['GROUP_LEVEL_ID'];
+					//echo "Group Lv name>".$lvName;
+					//echo "Group Lv id>".$lvId;
+					$lv1 = 0;
+					$lv2 = 0;
+					$lv3 = 0;
+					$lv4 = 0;
+					
+					if($lvName == 2){
+						switch ($lvId) {
+						// lv1 = 1
+						case 1:	
+							$lv1 = 1;	
+							$lv2 = $lvId;
+						    //echo "lv1  = $lv1"."<br>";
+						    break;
+						case 2:
+							$lv1 = 1;
+							$lv2 = $lvId;
+						    // echo "lv1  = $lv1"."<br>";
+						    break;
+						case 3:
+							$lv1 = 1;
+							$lv2 = $lvId;
+						    //echo "lv1  = $lv1"."<br>";
+						    break;
+						case 10:
+							$lv1 = 1;
+						    //echo "lv1  = $lv1"."<br>";
+						    break;
+						// lv1 = 2
+						case 4:	
+							$lv1 = 2;
+							$lv2 = $lvId;
+						    //echo "lv1  = $lv1"."<br>";
+						    break;
+						case 5:
+							$lv1 = 2;
+							$lv2 = $lvId;
+						    //echo "lv1  = $lv1"."<br>";
+						    break;
+						case 6:
+							$lv1 = 2;
+							$lv2 = $lvId;
+						    //echo "lv1  = $lv1"."<br>";
+						    break;
+						case 7:
+							$lv1 = 2;
+							$lv2 = $lvId;
+						    //echo "lv1  = $lv1"."<br>";
+						    break;
+						case 8:
+							$lv1 = 2;
+							$lv2 = $lvId;
+						    //echo "lv1  = $lv1"."<br>";
+						    break;
+						case 9:
+							$lv1 = 2;
+							$lv2 = $lvId;
+						    //echo "lv1  = $lv1"."<br>";
+						    break;
+						}
+					}// end group level name = 2
+					
+					if($lvName == 3){
+						switch ($lvId) {
+						// lv2 = 3
+						case 3:	
+							$lv3 = $lvId;
+							$lv2 = 2;
+							$lv1 = 1;	
+						    // echo "lv2  = $lv2"."<br>";
+						    // echo "lv1  = $lv1"."<br>";
+						    break;
+						case 4:
+							$lv3 = $lvId;
+							$lv2 = 2;
+							$lv1 = 1;
+						    // echo "lv2  = $lv2"."<br>";
+						    // echo "lv1  = $lv1"."<br>";
+						    break;
+						case 5:
+							$lv3 = $lvId;
+							$lv2 = 2;
+							$lv1 = 1;
+						    // echo "lv2  = $lv2"."<br>";
+						    // echo "lv1  = $lv1"."<br>";
+						    break;
+						case 6:
+							$lv3 = $lvId;
+							$lv2 = 2;
+							$lv1 = 1;
+						    // echo "lv2  = $lv2"."<br>";
+						    // echo "lv1  = $lv1"."<br>";
+						    break;		
+						case 7:	
+							$lv3 = $lvId;
+							$lv2 = 2;
+							$lv1 = 1;
+						    // echo "lv2  = $lv2"."<br>";
+						    // echo "lv1  = $lv1"."<br>";
+						    break;
+						case 8:
+							$lv3 = $lvId;
+							$lv2 = 2;
+							$lv1 = 1;
+						    // echo "lv2  = $lv2"."<br>";
+						    // echo "lv1  = $lv1"."<br>";
+						    break;
+						// lv2 = 2
+						case 9:
+							$lv3 = $lvId;
+							$lv2 = 2;
+							$lv1 = 1;
+						    // echo "lv2  = $lv2"."<br>";
+						    // echo "lv1  = $lv1"."<br>";
+						    break;
+						case 10:
+							$lv3 = $lvId;
+							$lv2 = 2;
+							$lv1 = 1;
+						    // echo "lv2  = $lv2"."<br>";
+						    // echo "lv1  = $lv1"."<br>";
+						    break;
+						case 11:
+							$lv3 = $lvId;
+							$lv2 = 2;
+							$lv1 = 1;
+						    // echo "lv2  = $lv2"."<br>";
+						    // echo "lv1  = $lv1"."<br>";
+						    break;
+						case 12:
+							$lv3 = $lvId;
+							$lv2 = 4;
+							$lv1 = 1;
+						    // echo "lv2  = $lv2"."<br>";
+						    // echo "lv1  = $lv1"."<br>";
+						    break;
+						}
+					}// end group level name = 3
+					
+					if($lvName == 4){
+						switch ($lvId) {
+						// lv2 = 3
+						case 5:
+							$lv4 = $lvId;
+							$lv3 = 10;
+							$lv2 = 3;
+							$lv1 = 1;		
+						    // echo "lv3  = $lv3"."<br>";
+						    // echo "lv2  = $lv2"."<br>";
+							// echo "lv1  = $lv1"."<br>";
+						    break;
+						case 6:
+							$lv4 = $lvId;
+							$lv3 = 10;
+							$lv2 = 3;
+							$lv1 = 1;
+						    // echo "lv3  = $lv3"."<br>";
+						    // echo "lv2  = $lv2"."<br>";
+							// echo "lv1  = $lv1"."<br>";
+						    break;
+						case 7:
+							$lv4 = $lvId;
+							$lv3 = 11;
+							$lv2 = 3;
+							$lv1 = 1;
+						    // echo "lv3  = $lv3"."<br>";
+						    // echo "lv2  = $lv2"."<br>";
+							// echo "lv1  = $lv1"."<br>";
+						    break;
+						case 8:
+							$lv4 = $lvId;
+							$lv3 = 12;
+							$lv2 = 4;
+							$lv1 = 2;
+						    // echo "lv3  = $lv3"."<br>";
+						    // echo "lv2  = $lv2"."<br>";
+							// echo "lv1  = $lv1"."<br>";
+						    break;
+						
+						}
+					}// end group level name = 4
+					 // echo "lv4  = $lv4"."<br>";
+					 // echo "lv3  = $lv3"."<br>";
+					 // echo "lv2  = $lv2"."<br>";
+					 // echo "lv1  = $lv1"."<br>";
+					 
+					 $gLv4Id = $lv4;
+					 $gLv3Id = $lv3;
+					 $gLv2Id = $lv2;
+					 $gLv1Id = $lv1;
+					 
+					 // echo "$gLv1Id";
+					 // echo "$gLv2Id";
+					 // echo "$gLv3Id";
+					 
+					 $resultLv2 = mysql_query("
+								  SELECT *
+								  FROM GROUP_LV2
+								  WHERE ID = '$gLv2Id'				
+			   					  ");
+			   	     while ($rowLv2 = mysql_fetch_array($resultLv2)) {
+			   	     	   $lvname = $rowLv2['NAME'];
+					 }
+					 
+				}
 			
 			// $result = $segment->get_segment_array("คำไทยภาษาไทย  หน่อยแน่  เทคโนโลยี");
 			// $word = count($result);
 			// echo $word;
+				// $originalDate = $row['UPDATE_DATE'];
+				// $newDate = date("M d, Y", strtotime($originalDate));
 				echo "<section>";
+				echo "$lvname - ". date("d F Y", strtotime($row['date']));
 				echo "<a href='report-detail.php?pdf_id=". $row['id'] ."&id=".$row['glvId']."&glvl=".$row['glvl']."' id='new-release'>";
 				echo "<h3>" . $row['name'] . "</h3>";
 				echo "</a>";
