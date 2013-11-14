29c8834e6bdf9b10395e426c10fb676cb24e0d96
nikom2532 <nikom2532@hotmail.com>
2013-11-14 16:05:28 +0700

    fix header

--- Site/admin/include/header.php
+++ Site/admin/include/header.php
@@ -21,35 +21,35 @@ if (!$db -> open()) {
 	<link href="css/style.css" rel="stylesheet" type="text/css">
 	<link href="css/reset.css" rel="stylesheet" type="text/css">
 
+  <!--Tag It -->
+  <link href="css/tagIt/jquery.tagit.css" rel="stylesheet" type="text/css">
+  <link href="css/tagIt/tagit.ui-zendesk.css" rel="stylesheet" type="text/css">
+ 	<link href="css/tagIt/jquery-ui.css" rel="stylesheet" type="text/css">
+ 	<link href="css/jquery_notification.css" rel="stylesheet" type="text/css">
+
+
 	<script src="js/jquery-1.7.1.min.js"></script>
-	<script src="js/script.js"></script>
-	<script src="../js/scripts.js"></script>
 	
-    <!--Tag It -->
-    <!-- <link href="css/tagIt/jquery.tagit.css" rel="stylesheet" type="text/css">
-    <link href="css/tagIt/tagit.ui-zendesk.css" rel="stylesheet" type="text/css">
-   	<link href="css/tagIt/jquery-ui.css" rel="stylesheet" type="text/css">
-    <script src="js/tag-it.js" type="text/javascript" charset="utf-8"></script> -->
-    
-      <!--Tag It -->
-    <link href="css/tagIt/jquery.tagit.css" rel="stylesheet" type="text/css">
-    <link href="css/tagIt/tagit.ui-zendesk.css" rel="stylesheet" type="text/css">
-   	<link href="css/tagIt/jquery-ui.css" rel="stylesheet" type="text/css">
-   	<link href="css/jquery_notification.css" rel="stylesheet" type="text/css"> 
-    <script src="js/jquery-ui.min.js" type="text/javascript" charset="utf-8"></script>
-    <script src="js/tag-it.js" type="text/javascript" charset="utf-8"></script>
-    
+  <!--Tag It -->
+  <!-- <link href="css/tagIt/jquery.tagit.css" rel="stylesheet" type="text/css">
+  <link href="css/tagIt/tagit.ui-zendesk.css" rel="stylesheet" type="text/css">
+ 	<link href="css/tagIt/jquery-ui.css" rel="stylesheet" type="text/css">
+  <script src="js/tag-it.js" type="text/javascript" charset="utf-8"></script> -->
+  
 	<!--Date picker -->
-	<script src="js/jquery-ui.min.js"></script>
+	<link href="css/jquery-ui.css" rel="stylesheet" type="text/css">
+ 	 
+  <script src="js/jquery-ui.min.js" type="text/javascript" charset="utf-8"></script>
+  <script src="js/tag-it.js" type="text/javascript" charset="utf-8"></script>
 	<script src="js/date-picker/jquery.ui.core.js"></script>
 	<script src="js/date-picker/jquery.ui.datepicker.js"></script>
 	<script src="js/date-picker/jquery.ui.widget.js"></script>   
-	<link href="css/jquery-ui.css" rel="stylesheet" type="text/css">
 	
 	<script src="js/jquery_notification_v.1.js"></script>
-	<script src="js/jquery.validate.min.js"></script>
-	<!-- <script src="js/jquery_v_1.4.js"></script>  -->
+	<script src="../js/jquery.validate.min.js"></script>
 	
+	<script src="js/script.js"></script>
+	<script src="../js/scripts.js"></script>
 </head>
 <body>
 
