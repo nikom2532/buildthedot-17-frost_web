3a6ed4566ecbecf1befaeba388892d7928faf0a4
nikom2532 <arming@buildthedot.com>
2013-12-09 16:14:42 +0700

    reformat

--- Site/admin/group.php
+++ Site/admin/group.php
@@ -16,30 +16,22 @@ include("include/header-with-tabs.php");
 ?>
 	<!-- MAIN CONTENT -->
 	<div id="content">
-		
 		<div class="page-full-width cf">
-
 			<div class="content-module">
-			
 				<div class="content-module-heading cf">
-				
-					<h3 class="fl">PDF</h3>
+					<h3 class="fl">Group</h3>
 					<span class="fr expand-collapse-text">Click to collapse</span>
 					<span class="fr expand-collapse-text initial-expand">Click to expand</span>
-				
 				</div> <!-- end content-module-heading -->
-				
-				
 				<div class="content-module-main">
 					<div id="wrap-add-customer">
 						<a href="upload-pdf.php" class="round button orange ic-add image-left" >Upload PDF</a>
-                    </div>
-					
+          </div>
 					<table class="fixed">
 						<col width="2em" />
-	    				<col width="20em" />
-	    				<col width="5em" />
-	    				<col width="5em" />
+    				<col width="20em" />
+    				<col width="5em" />
+    				<col width="5em" />
 					
 						<thead>
 					
@@ -53,7 +45,7 @@ include("include/header-with-tabs.php");
 						</thead>
 						
 						<tbody>
-						<?php 
+<?php 
 							/*---------Paging------------*/	
 							$i=$_GET['i'];
 							$page=1;//Default page
@@ -65,16 +57,18 @@ include("include/header-with-tabs.php");
 							}
 							$start=($page-1)*$limit;
 							/*---------end Paging------------*/	
-							$strQuery ="SELECT p.ID AS id, 
-										p.NAME AS name,
-										a.GROUP_LEVEL_NAME AS glvName,
-										a.GROUP_LEVEL_ID AS glvId,
-										p.UPDATE_DATE AS updateDate
-										FROM PDF AS p 
-										INNER JOIN PDF_CATEGORY AS a
-										ON p.ID = a.PDF_ID
-										WHERE IS_ASIAN_COUNTRY = '0'
-										ORDER BY ID DESC ";
+							$strQuery ="
+								SELECT p.ID AS id, 
+								p.NAME AS name,
+								a.GROUP_LEVEL_NAME AS glvName,
+								a.GROUP_LEVEL_ID AS glvId,
+								p.UPDATE_DATE AS updateDate
+								FROM PDF AS p 
+								INNER JOIN PDF_CATEGORY AS a
+								ON p.ID = a.PDF_ID
+								WHERE IS_ASIAN_COUNTRY = '0'
+								ORDER BY ID DESC 
+							;";
 							$result = mysql_query($strQuery);	
 							$Num_Rows = mysql_num_rows($result);
 							$strQuery .= "LIMIT $start , $limit";
@@ -83,18 +77,16 @@ include("include/header-with-tabs.php");
 							if($page ==1){
 								$i = 1;
 							}
-							
 							while ($row = mysql_fetch_array($result)) {
-						?>
-						
+?>
 							<tr>
 								<td><?=$i ?></td>
 								<td><?=$row['name'] ?></td>
-								<?php
+<?php
 								$originalDate = $row['updateDate'];
 								$newDate = date("M d, Y", strtotime($originalDate));
 								echo "<td>" . $newDate . "</td>";
-								?>
+?>
 
 								<td>
 									<form method='post' action='edit-pdf.php' id='submitform' name='submitform'>
@@ -111,28 +103,18 @@ include("include/header-with-tabs.php");
 									</form>
 								</td>
 							</tr>
-						
-						<?php
+<?php
 							$i = $i + 1;
-
 							}
-						?>
-							
+?>
 						</tbody>
-						
 					</table>
-
-							<?php
-							echo pagination($limit, $page, "pdf.php?page=", $Num_Rows);
-							//call function to show pagination
-							?>
-
+<?php
+					echo pagination($limit, $page, "pdf.php?page=", $Num_Rows);
+					//call function to show pagination
+?>
 				</div> <!-- end content-module-main -->
-			
 			</div> <!-- end content-module -->
-		
 		</div> <!-- end full-width -->
-			
 	</div> <!-- end content -->
-
 <?php include("include/footer.php");?>
