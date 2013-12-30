769e340b85c7b6b36b5f2cca327b1731899d323e
nikom2532 <arming@buildthedot.com>
2013-12-31 05:09:28 +0700

    Fix country profile

--- Sites/Portal/report-detail.php
+++ Sites/Portal/report-detail.php
@@ -148,7 +148,7 @@ include ("include/top-menu.php");
 <?php 
 							if($_GET["id"]==11 && $_GET["glvl"]==3){
 ?>
-								<img src="images/coutries/<?php echo $row["PHOTO_NAME"]; ?>" width="250" height="172" /><br />
+								<img src="images/pdf_image/<?php echo $row["PHOTO_NAME"]; ?>" width="250" height="172" /><br />
 <?php
 							}else{
 							// elseif(!(
