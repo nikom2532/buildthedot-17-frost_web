0507c6814d4288b3091161a5b2dfdbfe3437e255
arming <nikom2532@gmail.com>
2014-02-19 17:29:37 +0700

    Backend - Customer Download View

--- Sites/Portal/admin/customer-download.php
+++ Sites/Portal/admin/customer-download.php
@@ -83,9 +83,6 @@ include("include/header-with-tabs.php");
 			<!-- end content-module-heading -->
 
 			<div class="content-module-main">
-				<div id="wrap-add-customer">
-					<a href="add-customer.php" class="round button orange ic-add image-left" >Add customer</a>
-				</div>
 
 				<table class="fixed">
 					<col width="2em" />
@@ -99,12 +96,10 @@ include("include/header-with-tabs.php");
 
 						<tr>
 							<th>No.</th>
-							<th>Name</th>
-							<th>Company</th>
 							<th>Username</th>
-							<th>Download Status</th>
-							<th>Status</th>
-							<th>Actions</th>
+							<th>Name Surname</th>
+							<th>PDF</th>
+							<th>Download Datetime</th>
 						</tr>
 
 					</thead>
@@ -123,15 +118,10 @@ include("include/header-with-tabs.php");
 						}
 						$start=($page-1)*$limit;
 						/*---------end Paging------------*/	
-						$strQuery = "SELECT DISTINCT 
-									a.ID AS id,
-									a.FIRSTNAME AS firstname,
-									a.ID AS userId,
-									a.LASTNAME AS lastname,
-									a.COMPANY AS company,
-									a.EMAIL AS email,
-									a.IS_ACTIVE AS userActive
-									FROM USER_PROFILE AS a ";
+						$strQuery = "
+							SELECT * 
+							FROM  `DOWNLOAD_STATISTICS` ;
+						";
 						$result = mysql_query($strQuery);
 						$Num_Rows = mysql_num_rows($result);
 						$strQuery .= "LIMIT $start , $limit";
@@ -205,115 +195,9 @@ include("include/header-with-tabs.php");
 					</tbody>
 
 				</table>
-				<?php
-						echo pagination($limit, $page, "customer.php?page=", $Num_Rows);
-				?>
-			</div>
-			<!-- end content-module-main -->
-
-		</div>
-		<!-- end content-module -->
-
-	</div>
-	<!-- end full-width -->
-
-	<?php //####################################################################### ?>
-
-	<div class="page-full-width cf">
-
-		<div class="content-module">
-
-			<div class="content-module-heading cf">
-
-				<h3 class="fl">Customer - Download</h3>
-				<span class="fr expand-collapse-text">Click to collapse</span>
-				<span class="fr expand-collapse-text initial-expand">Click to expand</span>
-
-			</div>
-			<!-- end content-module-heading -->
-
-			<div class="content-module-main">
-
-				<table class="fixed">
-					<col width="2em" />
-    				<col width="8em" />
-    				<col width="8em" />
-    				<col width="7em" />
-    				<col width="5em" />
-    				<col width="5em" />
-    				<col width="6em" />
-					<thead>
-
-						<tr>
-							<th>No.</th>
-							<th>Name</th>
-							<th>Company</th>
-							<th>Username</th>
-							<th>Actions</th>
-						</tr>
-
-					</thead>
-
-					<tbody>
-
-						<?php
-						/*---------Paging------------*/	
-						$i=$_GET['i'];
-						$page=1;//Default page
-						$limit=10;//Records per page
-						$start=0;//starts displaying records from 0
-						if(isset($_GET['page']) && $_GET['page']!=''){
-						$page=$_GET['page'];
-						$i=$page*10;			
-						}
-						$start=($page-1)*$limit;
-						/*---------end Paging------------*/	
-						$strQuery = "SELECT DISTINCT 
-									a.ID AS id,
-									a.FIRSTNAME AS firstname,
-									a.ID AS userId,
-									a.LASTNAME AS lastname,
-									a.COMPANY AS company,
-									a.EMAIL AS email,
-									a.IS_ACTIVE AS userActive
-									FROM USER_PROFILE AS a ";
-						$result = mysql_query($strQuery);
-						$Num_Rows = mysql_num_rows($result);
-						$strQuery .= "LIMIT $start , $limit";
-						//echo $strQuery;
-						$result = mysql_query($strQuery);
-							
-						if($page ==1){
-								$i = 1;
-						}
-							//qi soft - 
-						while ($row = mysql_fetch_array($result)) {
-?>
-							<tr>
-							<td><?=$i?></td>
-							<td><?=$row['firstname'] . "&nbsp;" . $row['lastname']?></td>
-							<td><?=$row['company']?></td>
-							<td><?=$row['email']?></td>
-
-								<td>
-									<form method='post' action='customer-download.php' id='submitform' name='submitform'>
-										<input type='hidden' name='userId' value="<?=$row['userId']?>">
-										<INPUT TYPE='image' class='left' SRC='images/icons/table/actions-edit.png' BORDER='0' style='margin:5px 10px 5px 55px;' ALT='VIEW Download'>
-									</form>
-								</td>
-
-							</tr>
-						<?php 
-							$i = $i + 1;
-						}
-						?>
-						
-					</tbody>
-
-				</table>
-				<?php
-						echo pagination($limit, $page, "customer.php?page=", $Num_Rows);
-				?>
+					<?php
+							echo pagination($limit, $page, "customer.php?page=", $Num_Rows);
+					?>
 			</div>
 			<!-- end content-module-main -->
 
